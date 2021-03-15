b849592912f7397fa6243e5c78017edc209570cd
Bogdan <m0980701299@gmail.com>
2021-03-15 21:04:51 +0200

    Fix ec2 vars
    Signed-off-by: Bogdan <m0980701299@gmail.com>

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -6,7 +6,6 @@ output "instance2_id" {
   value = "${element(aws_instance.my-test-instance.*.id, 2)}"
 }
 
-
 output "server_ip" {
   value = "${join(",",aws_instance.my-test-instance.*.public_ip)}"
 }
