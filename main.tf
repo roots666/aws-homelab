c2ab20560e3817f54bedade49c1eef50ae3047a5
Bogdan <m0980701299@gmail.com>
2021-03-15 21:04:51 +0200

    Fix ec2 vars
    Signed-off-by: Bogdan <m0980701299@gmail.com>

--- main.tf
+++ main.tf
@@ -22,8 +22,8 @@ module "alb" {
   source = "./alb"
   vpc_id = "${module.vpc.vpc_id}"
 
-  instance1_id = "${module.ec2.instance1_id}"
-  instance2_id = "${module.ec2.instance2_id}"
+  instance1_id = "${module.ec2.my-instance-1}"
+  instance2_id = "${module.ec2.my-instance-2}"
   subnet1 = "${module.vpc.subnet1}"
   subnet2 = "${module.vpc.subnet2}"
 }
